2c242a54ce9f52732791ee3893787d3c597d4130
Dana Mulyana <45911464+danamulyana@users.noreply.github.com>
2022-11-14 13:26:18 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,136 @@
+import { createStore } from './redux';
+
+function addTodoActionCreator({ id, text }) {
+  return {
+    type: 'ADD_TODO',
+    payload: {
+      id,
+      text,
+      complate: false
+    }
+  };
+}
+
+function deleteTodoActionCreator(id) {
+  return {
+    type: 'DELETE_TODO',
+    payload: {
+      id
+    }
+  };
+}
+
+function toggleTodoActionCreator(id) {
+  return {
+    type: 'TOGGLE_TODO',
+    payload: {
+      id
+    }
+  };
+}
+
+function todosReducer(todos = [], action = {}) {
+  if (action.type === 'ADD_TODO') {
+    return [...todos, action.payload];
+  }
+
+  if (action.type === 'DELETE_TODO') {
+    return todos.filter((todo) => todo.id !== action.payload.id);
+  }
+
+  if (action.type === 'TOGGLE_TODO') {
+    return todos.map((todo) => {
+      if (todo.id === action.payload.id) {
+        return { ...todo, complate: !todo.complate };
+      }
+
+      return todo;
+    });
+  }
+
+  return todos;
+}
+
+function addGoalActionCreator({ id, text }) {
+  return {
+    type: 'ADD_GOAL',
+    payload: {
+      id,
+      text
+    }
+  };
+}
+
+function deleteGoalActionCreator(id) {
+  return {
+    type: 'DELETE_GOAL',
+    payload: {
+      id
+    }
+  };
+}
+
+function goalsReducer(goals = [], action = {}) {
+  if (action.type === 'ADD_GOAL') {
+    return [...goals, action.payload];
+  }
+
+  if (action.type === 'DELETE_GOAL') {
+    return goals.filter((goal) => goal.id !== action.payload.id);
+  }
+
+  return goals;
+}
+
+function rootReducer(state = {}, action = {}) {
+  return {
+    todos: todosReducer(state.todos, action),
+    goals: goalsReducer(state.goals, action)
+  };
+}
+
+// consume
+const store = createStore(rootReducer);
+
+//subscribe state changed
+store.subscribe(() => {
+  console.log('state changed', store.getState());
+});
+
+store.dispatch(
+  addTodoActionCreator({
+    id: 1,
+    text: 'first'
+  })
+);
+store.dispatch(
+  addTodoActionCreator({
+    id: 2,
+    text: 'learn redux'
+  })
+);
+store.dispatch(
+  addTodoActionCreator({
+    id: 3,
+    text: 'learn three'
+  })
+);
+store.dispatch(deleteTodoActionCreator(2));
+
+store.dispatch(toggleTodoActionCreator(1));
+
+store.dispatch(
+  addGoalActionCreator({
+    id: 1,
+    text: 'Get a Doctorate'
+  })
+);
+
+store.dispatch(
+  addGoalActionCreator({
+    id: 2,
+    text: 'Be an Entrepreneur'
+  })
+);
+
+store.dispatch(deleteGoalActionCreator(1));
---
+++ src/redux.js
@@ -0,0 +1,35 @@
+function createStore(reducer) {
+  /**
+   * Store memiliki 4 hal
+   * 1. State
+   * 2. Mendapatkan state
+   * 3. Men-subscribe perubahan state
+   * 4. Memperbarui state
+   */
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter((listenerItem) => listenerItem !== listener);
+    };
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  //  the store
+  return {
+    getState,
+    subscribe,
+    dispatch
+  };
+}
+
+export { createStore };
